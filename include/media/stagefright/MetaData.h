bcdae3c3caf35dab301e3b7601fe01d1e711dc27
Steve Kondik <steve@cyngn.com>
2016-01-05 16:45:30 -0800

    stagefright: Add deferred release option
     * Fill in stub methods to improve video encoding performance.
    
    Change-Id: I2bbf192e022043d161429c6cabf4a7d740802d20
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- include/media/stagefright/MetaData.h
+++ include/media/stagefright/MetaData.h
@@ -230,6 +230,10 @@ enum {
     kKeyTemporalLayerCount = 'cLyr', // int32_t, number of temporal layers encoded
     
     kKeyArbitraryMode     = 'ArbM',
+
+    // Indicate if it is OK to hold on to the MediaBuffer and not
+    // release it immediately
+    kKeyCanDeferRelease   = 'drel', // bool (int32_t)
 };
 
 enum {
