750e643827507bd51d46ca59872008308b8f3d56
Steve Kondik <steve@cyngn.com>
2016-08-30 14:37:49 -0700

    stagefright: Improve FLAC and WAV support for 24-bit
     -- Adjusted for CAF / XOS --
    
     * Forward-port changes from CM11
     * Add PCM encoding helpers
     * Remove truncation support as AudioFlinger handles
       rebuffering in case it can't go to the hardware.
    
    Change-Id: Iad30d04ee051050e444c3d665fa8bb7a1cfef348
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>
    
     Conflicts:
    	include/media/stagefright/MediaDefs.h
    	media/libstagefright/FLACExtractor.cpp
    	media/libstagefright/WAVExtractor.cpp
    
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- media/libstagefright/WAVExtractor.cpp
+++ media/libstagefright/WAVExtractor.cpp
@@ -296,7 +296,7 @@ status_t WAVExtractor::init() {
                             mTrackMeta->setInt32(kKeyPcmEncoding, kAudioEncodingPcmFloat);
                         } else {
                             mTrackMeta->setInt32(kKeyPcmEncoding,
-                                    getAudioEncodingFromBits(mBitsPerSample));
+                                    bitsToAudioEncoding(mBitsPerSample));
                         }
                         break;
                     case WAVE_FORMAT_ALAW:
